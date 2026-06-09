edbbacc0d8be0d27ae097f891404157ec1588042
Candy-649 <a199155@siswa.ukm.edu.my>
2026-06-09 15:10:35 +0800

    Delete A199155_HUANGXIGE_Dr.Nazatul_Project2 directory

--- A199155_HUANGXIGE_Dr.Nazatul_Project2/app/src/main/java/com/example/healthyhabittracker/data/FirebaseStepData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.healthyhabittracker.data
-
-data class FirebaseStepData(
-    val userId: String = "",
-    val date: String = "",
-    val steps: Int = 0,
-    val goal: Int = 0,
-    val timestamp: Long = 0
-)
